9087c76032541aa0d398973c716ba9fca56c923b
Mauro <mauro.kinjuk@gmail.com>
2023-08-09 20:51:23 -0300

    Juego completado

--- juego-adivinanzas.py
+++ juego-adivinanzas.py
@@ -1,39 +1,55 @@
 #Día 6-7: Juego de Adivinanzas
 #Desarrolla un juego donde el programa elige un número aleatorio y el usuario debe adivinarlo. Proporciona pistas para guiar al usuario hacia la respuesta correcta.
 import random
+import time
+from os import system
 
-def genNumeroAleatorio():
+def limpiarConsola():
+    """
+    Función que espera 2 segundos y luego limpia la consola.
+    """
+    time.sleep(0.5)
+    system("cls")
+
+def genAleatorio(nMax):
+    return random.randrange(1,nMax)
+
+def juego():
+    print(f"Hola! Vamos a jugar un juego donde vas a tener que adivinar un numero")
     while True:
         try:
-            x = int(input("Ingrese numero maximo a adivinar: "))
+            nMax = int(input("Ingrese el numero maximo de generacion: "))
+            nIntentos = int(input("Ingrese el numero maximo de intentos: "))
             break
         except:
-            print("Debes elegir un numero entero.")
-        finally:
-            nRandom = random.randint(1,x)
-    return nRandom
-
-def ayudas(elec,nrandom):
-    if elec > nrandom:
-        print(f"El numero es menor a {elec}")
-    if elec < nrandom:
-        print(f"El numero es mayor a {elec}")
-    return True
-
-def adivinanza():
-    x = genNumeroAleatorio()
-    intentosMax = input("Cuantos intentos maximos deseas tener?")
-    print(x)
-    print(f"Debes adivinar el numero que se encuentra entre 1 y {x}")
+            print("Debe ser un numero.")
+    nAle = genAleatorio(nMax)
 
     contador = 0
-    while contador <= intentosMax:
-        elec1 = int(input("Escribe el numero:"))
-        if elec1 > x:
 
-
-    if elec1 != x:
-        ayudas(elec1,x)
-
-
-adivinanza()
+    while contador < nIntentos:
+        try:
+            x = int(input("Y el numero es: "))
+            if x == nAle:
+                print("mmm Estas seguro/a?...")
+                limpiarConsola()
+                print(f"Si! Felicidades! El numero aleatorio era {nAle}")
+                break
+            elif x < nAle:
+                limpiarConsola()
+                print(f"El numero es mayor a {x}")
+                contador += 1
+            elif x > nAle:
+                limpiarConsola()
+                print(f"El numero es menor a {x}")
+                contador += 1
+            if nIntentos-contador == 0:
+                print("No te quedan intentos :(")
+                print(f"El numero era: {nAle}")
+                break
+            print(f"Te quedan: {nIntentos-contador} intentos")
+        except:
+            limpiarConsola()
+            print("Debes escribir un numero.")
+    
+juego()
